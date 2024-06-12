a0b2a92c6a2da0f1f72f5c80d703d6182bee54fa
Gaurav Kumar <vermagaurav851@gmail.com>
2024-06-12 14:32:34 +0530

    text-size-fixing

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -65,7 +65,7 @@ function Page() {
           />
           <button className="px-8 py-2 rounded-full relative bg-slate-700 text-white text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600">
             <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
-            <span className="relative z-20">Submit</span>
+            <span className="relative z-20 text-lg font-medium">Submit</span>
           </button>
         </form>
       </div>
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -55,7 +55,7 @@ function FeaturedCourses() {
             <span className="absolute inset-0 overflow-hidden rounded-full">
               <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
             </span>
-            <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2.5 px-8 ring-1 ring-white/10">
+            <div className="relative flex space-x-2 text-lg font-medium items-center z-10 rounded-full bg-zinc-950 py-2.5 px-8 ring-1 ring-white/10">
               <span>{`View all Courses`}</span>
               <svg
             width="16"
